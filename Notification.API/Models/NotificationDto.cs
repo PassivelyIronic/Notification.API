daa53e6da916cf71cf517295ed6337f22c291041
PassivelyIronic <150748075+PassivelyIronic@users.noreply.github.com>
2025-05-05 23:10:27 +0200

    Added Dto

--- Notification.API/Models/NotificationDto.cs
+++ Notification.API/Models/NotificationDto.cs
@@ -1,6 +1,26 @@
-﻿namespace Notification.API.Models
+﻿using Notification.Api.Database;
+
+namespace Notification.Api.Models;
+
+public class NotificationDto
 {
-    public class NotificationDto
-    {
-    }
-}
+    public Guid Id { get; set; }
+
+    public string Recipient { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+
+    public bool SendEmail { get; set; }
+
+    public bool SendPush { get; set; }
+
+    public string TimeZone { get; set; } = "UTC";
+
+    public DateTime ScheduledTimeUtc { get; set; }
+
+    public DateTime CreatedAtUtc { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    // Map from entity
+    public static NotificationDto FromEntity(
